fa0a7c91172aee95068b5a794f51a1751f71fe11
Alexander Bias <abias@ssystems.de>
2026-09-24 11:44:32 +0200

    Release v5.0-r1

--- version.php
+++ version.php
@@ -25,8 +25,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_enrolkeycreator';
-$plugin->version = 2024100702;
-$plugin->release = 'v4.5-r3';
+$plugin->version = 2025041400;
+$plugin->release = 'v5.0-r1';
 $plugin->requires = 2025041400;
 $plugin->supported = [500, 500];
 $plugin->maturity = MATURITY_STABLE;
